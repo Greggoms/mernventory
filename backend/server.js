cd7496df20accbb2f0eaad264088fa421ef66240
Greg Burton <gpburton@ualr.edu>
2022-11-22 11:26:36 -0600

    renamed client to frontend

--- backend/server.js
+++ backend/server.js
@@ -34,11 +34,11 @@ app.use("/api/products", require("./routes/products"));
 
 // Serve frontend for production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "client", "build", "index.html")
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
     )
   );
 } else {
