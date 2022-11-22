e3411d4e4da7dcd22922c3e251e59f14aac13e77
Greg Burton <gpburton@ualr.edu>
2022-11-22 12:27:38 -0600

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5000",
-  "http://localhost:5000",
   "http://localhost:3000",
+  "http://localhost:5000",
+  "https://mernventory.cyclic.app",
+  "https://busy-pink-sparrow-coat.cyclic.app",
+  // "http://127.0.0.1:5000",
 ];
 
 module.exports = allowedOrigins;
