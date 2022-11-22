fecfc57a74bb1c9d7ee6a6a7b0d7739891602f96
Greg Burton <gpburton@ualr.edu>
2022-11-22 12:56:04 -0600

    adjusted base_url for prod
    adjust api calls from localhost to live domain

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+let BASE_URL;
+if (process.env.REACT_APP_NODE_ENV === "production") {
+  BASE_URL = process.env.REACT_APP_BASE_URL;
+} else {
+  BASE_URL = "http://localhost:5000/api";
+}
 
 export default axios.create({
   baseURL: BASE_URL,
